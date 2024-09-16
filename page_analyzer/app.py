a2cbc17c43fa5349e142f2316a7f4fe86d8528db
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-09-16 11:29:24 +0300

    edit after review

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -18,7 +18,7 @@ app.secret_key = os.getenv('SECRET_KEY')
 def main_page():
     messages = get_flashed_messages(with_categories=True)
     return render_template(
-        'main_page.html',
+        'index.html',
         messages=messages
     )
 
@@ -32,7 +32,7 @@ def add_url():
     if not validate_url(normalized_url):
         flash('Некорректный URL', 'danger')
         messages = get_flashed_messages(with_categories=True)
-        return render_template('main_page.html', messages=messages), 422
+        return render_template('index.html', messages=messages), 422
 
     url = db_tools.get_url_by('name', normalized_url)
     if url:
@@ -58,7 +58,7 @@ def url_page(id):
     url_checks = db_tools.get_url_checks(id)
     if not url:
         flash('Запрашиваемая страница не найдена', 'warning')
-        return redirect(url_for('main_page'), 404)
+        return redirect(url_for('index'), 404)
     return render_template('url_page.html', messages=messages,
                            url=url, url_checks=url_checks)
 
@@ -68,7 +68,7 @@ def check_url(id):
     url_name = db_tools.get_url_by('id', id).name
     if not url_name:
         flash('Запрашиваемая страница не найдена', 'warning')
-        return redirect(url_for('main_page'))
+        return redirect(url_for('index'))
 
     try:
         response = requests.get(url_name)
