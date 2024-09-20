fd65e82838525d759405e4baeb41812db83ad2c7
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-09-20 10:44:22 +0300

    fix app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -25,7 +25,7 @@ app.database_url = os.getenv("DATABASE_URL")
 @app.route("/")
 def main_page():
     messages = get_flashed_messages(with_categories=True)
-    return render_template('main_page.html', messages=messages)
+    return render_template("main_page.html", messages=messages)
 
 
 @app.post("/urls")
@@ -33,11 +33,12 @@ def add_url():
     connection = db_tools.db_connect(app)
 
     url_to_add = request.form.get("url")
+    parsed_url = urlparse(url_to_add)
     normalized_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
     if not validate_url(normalized_url):
         flash("Некорректный URL", "danger")
         messages = get_flashed_messages(with_categories=True)
-        return render_template('main_page.html', messages=messages), 422
+        return render_template("main_page.html", messages=messages), 422
 
     url = db_tools.get_url_by("name", normalized_url, connection=connection)
     if url:
@@ -45,7 +46,7 @@ def add_url():
         return redirect(url_for("url_page", id=url.id))
     url = db_tools.insert_url(normalized_url, connection)
     flash("Страница успешно добавлена", "success")
-    return redirect(url_for('url_page', id=url.id))
+    return redirect(url_for("url_page", id=url.id))
 
 
 @app.get("/urls")
@@ -53,7 +54,7 @@ def urls():
     connection = db_tools.db_connect(app)
     messages = get_flashed_messages(with_categories=True)
     urls = db_tools.get_all_urls(connection=connection)
-    return render_template('urls.html', messages=messages, urls=urls)
+    return render_template("urls.html", messages=messages, urls=urls)
 
 
 @app.get("/urls/<int:id>")
@@ -64,9 +65,9 @@ def url_page(id):
     url_checks = db_tools.get_url_checks(id, connection=connection)
     if not url:
         flash("Запрашиваемая страница не найдена", "warning")
-        return redirect(url_for('main_page'), 404)
+        return redirect(url_for("main_page"), 404)
     return render_template(
-        'url_page.html', messages=messages, url=url, url_checks=url_checks
+        "url_page.html", messages=messages, url=url, url_checks=url_checks
     )
 
 
@@ -77,17 +78,17 @@ def check_url(id):
     url_name = db_tools.get_url_by("id", id, connection=connection).name
     if not url_name:
         flash("Запрашиваемая страница не найдена", "warning")
-        return redirect(url_for('main_page'))
+        return redirect(url_for("main_page"))
 
     try:
         response = requests.get(url_name)
         response.raise_for_status()
     except requests.exceptions.RequestException:
         flash("Произошла ошибка при проверке", "danger")
-        return redirect(url_for('url_page', id=id))
+        return redirect(url_for("url_page", id=id))
 
     db_tools.add_url_check(
         id, url_parsing.get_url_data(response), connection=connection
     )
     flash("Страница успешно проверена", "success")
-    return redirect(url_for('url_page', id=id))
+    return redirect(url_for("url_page", id=id))
