51cbca8bffee7ddaa00c44a5eeabd047aaef29e7
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-01 10:57:43 +0300

    edit db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,7 +11,7 @@ import os
 from urllib.parse import urlparse
 from validators.url import url as validate_url
 from dotenv import load_dotenv
-from . import db_tools, url_parsing
+from . import db, url_parsing
 import requests
 
 load_dotenv()
@@ -41,13 +41,13 @@ def add_url():
         messages = get_flashed_messages(with_categories=True)
         return render_template("index.html", messages=messages), 422
 
-    with db_tools.db_connect(app) as connection:
-        url = db_tools.get_url_by("name", normalized_url, connection=connection)
+    with db.db_connect(app) as connection:
+        url = db.get_url_by("name", normalized_url, connection=connection)
         if url:
             flash("Страница уже существует", "warning")
             return redirect(url_for("url_page", id=url.id))
 
-        url = db_tools.insert_url(normalized_url, connection)
+        url = db.insert_url(normalized_url, connection)
 
     flash("Страница успешно добавлена", "success")
     return redirect(url_for("url_page", id=url.id))
@@ -56,17 +56,17 @@ def add_url():
 @app.get("/urls")
 def urls():
     messages = get_flashed_messages(with_categories=True)
-    with db_tools.db_connect(app) as connection:
-        urls = db_tools.get_all_urls(app, connection=connection)
+    with db.db_connect(app) as connection:
+        urls = db.get_all_urls(app, connection=connection)
     return render_template("urls.html", urls=urls, messages=messages)
 
 
 @app.get("/urls/<int:id>")
 def url_page(id):
     messages = get_flashed_messages(with_categories=True)
-    with db_tools.db_connect(app) as connection:
-        url = db_tools.get_url_by("id", id, connection=connection)
-        url_checks = db_tools.get_url_checks(id, connection=connection)
+    with db.db_connect(app) as connection:
+        url = db.get_url_by("id", id, connection=connection)
+        url_checks = db.get_url_checks(id, connection=connection)
 
         if not url:
             flash("Запрашиваемая страница не найдена", "warning")
@@ -79,8 +79,8 @@ def url_page(id):
 
 @app.post("/urls/<int:id>/checks")
 def check_url(id):
-    with db_tools.db_connect(app) as connection:
-        url_name = db_tools.get_url_by("id", id, connection=connection).name
+    with db.db_connect(app) as connection:
+        url_name = db.get_url_by("id", id, connection=connection).name
         if not url_name:
             flash("Запрашиваемая страница не найдена", "warning")
             return redirect(url_for("index"))
@@ -92,7 +92,7 @@ def check_url(id):
             flash("Произошла ошибка при проверке", "danger")
             return redirect(url_for("url_page", id=id))
 
-        db_tools.add_url_check(
+        db.add_url_check(
             id, url_parsing.get_url_data(response), connection=connection
         )
         flash("Страница успешно проверена", "success")
